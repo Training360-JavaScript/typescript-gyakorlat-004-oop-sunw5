ee458c288a9470181e177f380f415308caeac10f
Gabor Szasz <skywallalp@gmail.com>
2022-01-06 06:08:07 +0100

    solved

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import {HumanHero} from './hero';
+import {TransformerHero} from './transformer';
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,12 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: HumanHero[] = [
+  new HumanHero(1, 'Bob', 'male', 30, 50, 'photoBob'),
+  new HumanHero(2, 'Miriam', 'female', 25, 50, 'photoMiriam'),
+  new HumanHero(3, 'Joy', 'female', 30, 50, 'photoJoy'),
+
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +23,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} humans
  */
-export const transformers: any[] = [];
+export const transformers: TransformerHero[] = [
+  new TransformerHero(1, 'Tex', 2, 2, 'bluewing', 'photoTex'),
+  new TransformerHero(2, 'Wi', 2, 2, 'greenwing', 'photoWi'),
+  new TransformerHero(3, 'Hoppy', 2, 2, 'hoppers', 'photoHoppy'),
+];
--- src/transformer.ts
+++ src/transformer.ts
@@ -7,6 +7,20 @@ import { ITransformer } from './hero-interface';
  * Deklaráld benne az interfész által megkövetelt változókat.
  * Definiáld a konstruktort és kérj be minden változót, majd állítsd be őket.
  */
-export class TransformerHero {
-    
+export class TransformerHero implements ITransformer {
+    id: number;
+    name: string;
+    photo?: string = '';
+    wings: number;
+    wheels: number;
+    clan: string;
+
+    constructor(id: number, name: string, wings: number, wheels: number, clan: string , photo?: string) {
+      this.id = id;
+      this.name = name;
+      this.wings = wings;
+      this.wheels = wheels;
+      this.clan = clan;
+      this.photo = photo;
+    }
 }
